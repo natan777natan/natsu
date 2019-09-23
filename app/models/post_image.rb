83df16771f432f6e442d11362a70c57dcb3182bc
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2019-09-23 19:19:47 +0900

    post_images_index

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,6 @@
 class PostImage < ApplicationRecord
-    attachment :image
     belongs_to :user
+    attachment :image
     validates :shop_name, presence: true
     validates :image, presence: true
     
